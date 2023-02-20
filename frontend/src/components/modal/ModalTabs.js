15c15e32a4ef2547ae70c8bb59d8d932fc08590b
Stiven Kang <kdaegyung@gmail.com>
2023-02-20 10:59:11 -0500

    Added Signup Form

--- frontend/src/components/modal/ModalTabs.js
+++ frontend/src/components/modal/ModalTabs.js
@@ -9,6 +9,7 @@ import {
 	TabPanel,
 } from "@chakra-ui/react";
 import LoginForm from "../Auth/LoginForm";
+import SignupForm from "../Auth/SignupForm";
 // import LoginForm from "../authentication/LoginForm";
 // import NewAccountForm from "../authentication/NewAccountForm";
 // import ModalWelcomeHeader from "../header/Welcome";
@@ -21,8 +22,8 @@ const ModalTabs = ({ closeModal }) => {
 					borderBottom={"1px solid "}
 					borderColor={"rgb(209 209 213)"}
 				>
-					<Tab className="form-tab">Sign in</Tab>
-					<Tab>New Account</Tab>
+					<Tab className="form-tab">Log In</Tab>
+					<Tab>Sign Up</Tab>
 				</TabList>
 				<TabPanels>
 					<TabPanel>
@@ -30,7 +31,7 @@ const ModalTabs = ({ closeModal }) => {
 						{/* <LoginForm closeModalFunc={closeModal} /> */}
 					</TabPanel>
 					<TabPanel>
-						<h1>SignupForm</h1>
+						<SignupForm />
 						{/* <NewAccountForm closeModalFunc={closeModal} /> */}
 					</TabPanel>
 				</TabPanels>
