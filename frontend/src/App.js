6600554ef039ee4f19dea382638be83cd1ebed4a
Mlkz <maho.mamulashvili@gmail.com>
2023-02-19 20:21:35 -0500

    Add store and map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
+import Map from "./Map/Map";
 function App() {
-  return (
-    <h1>Hello from App</h1>
-  );
+	return (
+		<>
+			<h1>Hello from App</h1>
+			<Map />
+		</>
+	);
 }
 
 export default App;
