1a5e5616bfb04d7cf420bfcf27ee5c6ea2c71ca5
Mlkz <maho.mamulashvili@gmail.com>
2023-02-19 19:44:43 -0500

    Add user signup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const { loginUser, restoreUser } = require('../../config/passport');
 const { isProduction } = require('../../config/keys');
-// const validateRegisterInput = require('../../validations/register');
+const validateRegisterInput = require('../../validations/register');
 // const validateLoginInput = require('../../validations/login');
 const User = mongoose.model('User');
 
@@ -19,51 +19,51 @@ router.get('/', function(req, res, next) {
 });
 
 // POST /api/users/register
-// router.post('/register', validateRegisterInput, async (req, res, next) => {
-//   // Check to make sure no one has already registered with the proposed email or
-//   // username.
-//   const user = await User.findOne({
-//     $or: [{ email: req.body.email }, { username: req.body.username }]
-//   });
+router.post('/register', validateRegisterInput, async (req, res, next) => {
+  // Check to make sure no one has already registered with the proposed email or
+  // username.
+  const user = await User.findOne({
+    $or: [{ email: req.body.email }, { username: req.body.username }]
+  });
 
-//   if (user) {
-//     // Throw a 400 error if the email address and/or email already exists
-//     const err = new Error("Validation Error");
-//     err.statusCode = 400;
-//     const errors = {};
-//     if (user.email === req.body.email) {
-//       errors.email = "A user has already registered with this email";
-//     }
-//     if (user.username === req.body.username) {
-//       errors.username = "A user has already registered with this username";
-//     }
-//     err.errors = errors;
-//     return next(err);
-//   }
+  if (user) {
+    // Throw a 400 error if the email address and/or email already exists
+    const err = new Error("Validation Error");
+    err.statusCode = 400;
+    const errors = {};
+    if (user.email === req.body.email) {
+      errors.email = "A user has already registered with this email";
+    }
+    if (user.username === req.body.username) {
+      errors.username = "A user has already registered with this username";
+    }
+    err.errors = errors;
+    return next(err);
+  }
 
   // Otherwise create a new user
-//   const newUser = new User({
-//     username: req.body.username,
-//     email: req.body.email
-//   });
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email
+  });
 
-//   bcrypt.genSalt(10, (err, salt) => {
-//     if (err) throw err;
-//     bcrypt.hash(req.body.password, salt, async (err, hashedPassword) => {
-//       if (err) throw err;
-//       try {
-//         newUser.hashedPassword = hashedPassword;
-//         const user = await newUser.save();
-//         // return res.json({ user });
-//         return res.json(await loginUser(user)); // <-- THIS IS THE CHANGED LINE
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) throw err;
+    bcrypt.hash(req.body.password, salt, async (err, hashedPassword) => {
+      if (err) throw err;
+      try {
+        newUser.hashedPassword = hashedPassword;
+        const user = await newUser.save();
+        // return res.json({ user });
+        return res.json(await loginUser(user)); // <-- THIS IS THE CHANGED LINE
 
-//       }
-//       catch(err) {
-//         next(err);
-//       }
-//     })
-//   });
-// });
+      }
+      catch(err) {
+        next(err);
+      }
+    })
+  });
+});
 
 
 // POST /api/users/login
