a5d10221c0a5c6041be1a9716ac49a4e57718c82
Stiven Kang <kdaegyung@gmail.com>
2023-02-21 17:15:03 -0500

    Created SelectedStates component and changed css accordingly

--- frontend/src/components/Spot/CreateSpotForm.js
+++ frontend/src/components/Spot/CreateSpotForm.js
@@ -2,127 +2,127 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { createSpot } from "../../store/spots";
 import "./CreateSpotForm.scss";
+import SelectedState from "../SelectedStates/SelectedStates";
 
 const SpotForm = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const [formData, setFormData] = useState({
-		address: "",
-		zip: "",
-		city: "",
-		state: "",
-		size: "",
-		accessible: false,
-	});
+  const [formData, setFormData] = useState({
+    address: "",
+    zip: "",
+    city: "",
+    state: "",
+    size: "",
+    accessible: false,
+  });
 
-	const handleChange = (event) => {
-		let { name, value } = event.target;
-        value = value === "on" ? true: value;
+  const handleChange = (event) => {
+    let { name, value } = event.target;
+	// <option value="AR">AR</option>
+	if (name === '') {
+		name = "state"
+	}
+    value = value === "on" ? true : value;
 
-        // debugger
-		setFormData((formData) => ({
-			...formData,
-			[name]: value,
-		}));
-	};
+    // debugger
+    setFormData((formData) => ({
+      ...formData,
+      [name]: value,
+    }));
+  };
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-		try {
-			const newSpot = dispatch(createSpot(formData));
-			console.log("New Spot created:", newSpot);
-			setFormData({
-				address: "",
-				zip: "",
-				city: "",
-				state: "",
-				size: "",
-				accessible: false,
-			});
-		} catch (error) {
-			console.error("Failed to create Spot:", error);
-		}
-	};
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const newSpot = dispatch(createSpot(formData));
+      console.log("New Spot created:", newSpot);
+      setFormData({
+        address: "",
+        zip: "",
+        city: "",
+        state: "",
+        size: "",
+        accessible: false,
+      });
+    } catch (error) {
+      console.error("Failed to create Spot:", error);
+    }
+  };
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<h1 className='createSpotTitle'>Create a new Spot!</h1>
-			<label>
-				Title:
-				<input
-					type="text"
-					name="title"
-					value={formData.title}
-					onChange={handleChange}
-				/>
-			</label>
-			<label>
-				Rate:
-				<input
-					type="number"
-					name="rate"
-					value={formData.hourlyRate}
-					onChange={handleChange}
-				/>
-			</label>
-			<label>
-				Address:
-				<input
-					type="text"
-					name="address"
-					value={formData.address}
-					onChange={handleChange}
-				/>
-			</label>
-			<label>
-				City:
-				<input
-					type="text"
-					name="city"
-					value={formData.city}
-					onChange={handleChange}
-				/>
-			</label>
-			<label>
-				State:
-				<input
-					type="text"
-					name="state"
-					value={formData.state}
-					onChange={handleChange}
-				/>
-			</label>
-			<label>
-				Zip:
-				<input
-					type="text"
-					name="zip"
-					value={formData.zip}
-					onChange={handleChange}
-				/>
-			</label>
-			<label>
-				Size:
-				<input
-					type="text"
-					name="size"
-					value={formData.size}
-					onChange={handleChange}
-				/>
-			</label>
-			<label>
-				Accessible:
-				<input
-					type="checkbox"
-					name="accessible"
-					checked={formData.accessible}
-					onChange={handleChange}
-				/>
-			</label>
-			<button type="submit">Add Spot</button>
-		</form>
-	);
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1 className="createSpotTitle">Create a new Spot!</h1>
+      <label>
+        Title:
+        <input
+          type="text"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Rate:
+        <input
+          type="number"
+          name="rate"
+          value={formData.hourlyRate}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Address:
+        <input
+          type="text"
+          name="address"
+          value={formData.address}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        City:
+        <input
+          type="text"
+          name="city"
+          value={formData.city}
+          onChange={handleChange}
+        />
+      </label>
+	  <label className='selectState'>
+		<SelectedState 
+		state={formData.state}
+		handleChange={handleChange} />
+	  </label>
+      <label>
+        Zip:
+        <input
+          type="text"
+          name="zip"
+          value={formData.zip}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Car Type:
+        <input
+          type="text"
+          name="size"
+          value={formData.size}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Accessible:
+        <input
+          type="checkbox"
+          name="accessible"
+          checked={formData.accessible}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit">Add Spot</button>
+    </form>
+  );
 };
-
 
 export default SpotForm;
