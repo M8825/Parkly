a40f2ae8c050f80696551c3f1b30120e1a2ba278
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-02-21 14:04:52 -0500

    added phone number to model and validation

--- backend/models/User.js
+++ backend/models/User.js
@@ -15,6 +15,10 @@ const userSchema = new Schema(
 			type: String,
 			required: true,
 		},
+		phoneNumber: {
+			type: String,
+			required: true
+		}
 	},
 	{
 		timestamps: true,
--- backend/validations/register.js
+++ backend/validations/register.js
@@ -18,6 +18,11 @@ const validateRegisterInput = [
     .exists({ checkFalsy: true })
     .isLength({ min: 6, max: 30 })
     .withMessage('Password must be between 6 and 30 characters'),
+
+  check('phoneNumber')
+    .exists({checkFalsy: true})
+    .isLength(10)
+    .withMessage('Phone number must be a valid phone number'),
   handleValidationErrors
 ];
 
