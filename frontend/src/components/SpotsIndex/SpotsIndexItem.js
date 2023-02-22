3ae5c3dded40fd060c7f545818e7f4842b9ff527
Yen Lee <yenny0104@gmail.com>
2023-02-22 09:59:48 -0500

    Add index page styling

--- frontend/src/components/SpotsIndex/SpotsIndexItem.js
+++ frontend/src/components/SpotsIndex/SpotsIndexItem.js
@@ -1,16 +1,26 @@
 const SpotsIndexItem = ({ spot }) => {
 
     return (
-        <li>
-            <p>{spot.city}</p>
-            <p>{spot.state}</p>
-            <p>{spot.zip}</p>
-            <p>{spot.size}</p>
-            <p>{spot.accessibility}</p>
-            <p>{spot.rating}</p>
-            {/* <p>{spot.owner.firstName}</p> */}
-            {/* <p>{spot.owner.lastName}</p> */}
-        </li>
+
+        // <li>
+        //     <p>{spot.city}</p>
+        //     <p>{spot.state}</p>
+        //     <p>{spot.zip}</p>
+        //     <p>{spot.size}</p>
+        //     <p>{spot.accessibility}</p>
+        //     <p>{spot.rating}</p>
+        //     {/* <p>{spot.owner.firstName}</p> */}
+        //     {/* <p>{spot.owner.lastName}</p> */}
+        // </li>
+        <div className="card">
+          <div class="card-header">{spot.size}</div>
+            <div className="card-body">
+                <h5 className="card-title">{spot.title}</h5>
+                <p className="card-text">{spot.city} {spot.state} {spot.zipcode}</p>
+                <p className="card-text">{spot.rating}</p>
+                <a href="#" className="btn btn-primary see-btn">See more</a>
+            </div>
+        </div>
     )
 
 };
