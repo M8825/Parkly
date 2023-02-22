c1028dd8741e222cff4946d58bc086e29eaf5896
Yen Lee <yenny0104@gmail.com>
2023-02-22 15:16:55 -0500

    change button of index card

--- frontend/src/components/SpotsIndex/SpotsIndexItem.js
+++ frontend/src/components/SpotsIndex/SpotsIndexItem.js
@@ -2,13 +2,13 @@ const SpotsIndexItem = ({ spot }) => {
 
     return (
         <div className="card">
-          <div class="card-header">{spot.size}</div>
+            <div class="card-header">{spot.size}</div>
             <div className="card-body">
                 <h5 className="card-title">{spot.title}</h5>
                 <p className="card-text">{spot.address} {spot.city} {spot.state} {spot.zip}</p>
                 <p className="card-text">{spot.rating}</p>
                 <div className="btn-container">
-                <button href="#" className="btn btn-primary see-btn">See more</button>
+                    <button href="#" className="btn btn-primary see-btn">See more</button>
                 </div>
             </div>
         </div>
