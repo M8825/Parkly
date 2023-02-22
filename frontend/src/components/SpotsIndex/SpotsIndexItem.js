8bf7be3dc06ada42a861940bb48eb881768765f5
Yen Lee <yenny0104@gmail.com>
2023-02-22 10:25:25 -0500

    Fix address sting on index page card

--- frontend/src/components/SpotsIndex/SpotsIndexItem.js
+++ frontend/src/components/SpotsIndex/SpotsIndexItem.js
@@ -16,7 +16,7 @@ const SpotsIndexItem = ({ spot }) => {
           <div class="card-header">{spot.size}</div>
             <div className="card-body">
                 <h5 className="card-title">{spot.title}</h5>
-                <p className="card-text">{spot.city} {spot.state} {spot.zipcode}</p>
+                <p className="card-text">{spot.address} {spot.city} {spot.state} {spot.zip}</p>
                 <p className="card-text">{spot.rating}</p>
                 <a href="#" className="btn btn-primary see-btn">See more</a>
             </div>
