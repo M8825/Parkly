71968cf8137a6a44eb6c858ee46f2c67dc699fb8
Yen Lee <yenny0104@gmail.com>
2023-02-23 09:25:49 -0500

    finish styling show page

--- frontend/src/components/ShowPage/ShowPage.js
+++ frontend/src/components/ShowPage/ShowPage.js
@@ -18,11 +18,44 @@ const ShowPage = () => {
     <>
       <div className="show-bg">
         <div className="show-wrapper">
-          <div className="bottom-map">
-            <Map />
+          <div className="show-leftside">  
+            <div className="show-images">
+              <img src="https://sienaconstruction.com/wp-content/uploads/2017/05/test-image.jpg"></img>
+              <img src="https://sienaconstruction.com/wp-content/uploads/2017/05/test-image.jpg"></img>
+              <img src="https://sienaconstruction.com/wp-content/uploads/2017/05/test-image.jpg"></img>
+              <img src="https://sienaconstruction.com/wp-content/uploads/2017/05/test-image.jpg"></img>
+            </div>
+            <div className="map-container">
+            <Map />           
+            </div>
+            
+
+            
+              
           </div>
-          <div className="reserve-side">
-            <h1>{spot.address}</h1>
+          <div className="show-rightside">
+            
+            <div><h4>{spot.title}</h4><p className="owner">(firstname) (lastname)</p></div>
+            
+            <h5 className="show-address">{spot.address}. {spot.city}, {spot.state}</h5>
+            
+            <div className="show-infos">              
+                <p className="show-infos-info"><img src={require('./dollar.png')}/>  17.5/hr</p>
+                <p className="show-infos-info"><img src={require('./type.png')}/>  {spot.type}(Senda)</p>
+                <p className="show-infos-info"><img src={require('./reviews.png')}/>  (4.7)</p>
+                <p className="show-infos-info"><img src={require('./access.png')}/>  {spot.accessible}(No)</p>
+            </div>
+
+            <div>
+              Date Picker
+            </div>
+
+            <div className="show-reviews">
+              <h5> - 4.98 * 91 reviews</h5>
+
+            </div>
+
+
           </div>
         </div>
       </div>
