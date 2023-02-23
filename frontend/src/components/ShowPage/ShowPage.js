d875ea64c158c190bc0940de805c2dc33096a61d
Yen Lee <yenny0104@gmail.com>
2023-02-22 22:31:11 -0500

    set show page

--- frontend/src/components/ShowPage/ShowPage.js
+++ frontend/src/components/ShowPage/ShowPage.js
@@ -6,17 +6,29 @@ import Map from "../Map/Map";
 import './ShowPage.css'
 
 const ShowPage = () => {
-    const { spotId } = useParams();
-    const dispatch = useDispatch();
-    const spot = useSelector(getSpot(spotId));
-  
-    useEffect(() => {
-      dispatch(fetchSpot(spotId));
-    }, [dispatch]);
-    
-    return ( spot &&
-     <h1>{spot.address}</h1>   
-    )
+  const { spotId } = useParams();
+  const dispatch = useDispatch();
+  const spot = useSelector(getSpot(spotId));
+
+  useEffect(() => {
+    dispatch(fetchSpot(spotId));
+  }, [dispatch]);
+
+  return (spot && (
+    <>
+      <div className="show-bg">
+        <div className="show-wrapper">
+          <div className="bottom-map">
+            <Map />
+          </div>
+          <div className="reserve-side">
+            <h1>{spot.address}</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+  )
 }
 
 
