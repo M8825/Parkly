49c8e2cc42b1cad8cb29d2a387713372888f841b
Mlkz <55971701+M8825@users.noreply.github.com>
2023-02-28 16:17:53 -0500

    Fix App js

--- backend/app.js
+++ backend/app.js
@@ -26,13 +26,13 @@ app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
 
+app.use(passport.initialize()); // TODO: maybe move this to the if statement below
 // ADD THIS SECURITY MIDDLEWARE
 // Security Middleware
 if (!isProduction) {
 	// Enable CORS only in development because React will be on the React
 	// development server (http://localhost:3000). (In production, the Express
 	// server will serve the React files statically.)
-	app.use(passport.initialize()); // TODO: maybe move this to the if statement below
 	app.use(cors());
 }
 
@@ -54,29 +54,6 @@ app.use("/api/users", usersRouter); // update the path
 app.use("/api/csrf", csrfRouter);
 app.use("/api/spots", spotsRouter);
 app.use("/api/reservations", reservationsRouter);
-
-// Express custom middleware for catching all unmatched requests and formatting
-// a 404 error to be sent as the response.
-app.use((req, res, next) => {
-	const err = new Error("Not Found");
-	err.statusCode = 404;
-	next(err);
-});
-
-const serverErrorLogger = debug("backend:error");
-
-// Express custom error handler that will be called whenever a route handler or
-// middleware throws an error or invokes the `next` function with a truthy value
-app.use((err, req, res, next) => {
-	serverErrorLogger(err);
-	const statusCode = err.statusCode || 500;
-	res.status(statusCode);
-	res.json({
-		message: err.message,
-		statusCode,
-		errors: err.errors,
-	});
-});
 
 // Serve static React build files statically in production
 if (isProduction) {
@@ -100,5 +77,28 @@ if (isProduction) {
 		);
 	});
 }
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
+app.use((req, res, next) => {
+	const err = new Error("Not Found");
+	err.statusCode = 404;
+	next(err);
+});
+
+const serverErrorLogger = debug("backend:error");
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+	serverErrorLogger(err);
+	const statusCode = err.statusCode || 500;
+	res.status(statusCode);
+	res.json({
+		message: err.message,
+		statusCode,
+		errors: err.errors,
+	});
+});
+
 
 module.exports = app;
