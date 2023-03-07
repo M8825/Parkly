7a7f5499cd638a4bf500749dbb419249cd0d5a89
Yen Lee <yenny0104@gmail.com>
2023-03-07 00:30:01 -0500

    search bar done with button

--- frontend/src/components/SpotsIndex/SpotsIndex.js
+++ frontend/src/components/SpotsIndex/SpotsIndex.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSpots, getSpots } from "../../store/spots";
 import Map from "../Map/Map";
-import './SpotsIndex.css'
+import './SpotsIndex.scss'
 
 import SpotsIndexItem from "./SpotsIndexItem";
 
@@ -21,45 +21,33 @@ const SpotsIndex = () => {
           <div className="map-wrapper">
             <div>
               <div className="search-bar">
-                {/* <div class="input-group">
-                  <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-                  <button type="button" class="btn btn-outline-primary">search</button>
-                </div> */}
-
-                <div class="input-group mb-3">
-                  <button type="button" class="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                    Car Type<span class="visually-hidden">Toggle Dropdown</span>
+                <div className="input-group mb-3">
+                  <button type="button" className="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                    Car Type<span className="visually-hidden">Toggle Dropdown</span>
                   </button>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Sedan</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    <li><a class="dropdown-item" href="#">Separated link</a></li>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Sedan</a></li>
+                    <li><a className="dropdown-item" href="#">Truck</a></li>
+                    <li><a className="dropdown-item" href="#">Minivan</a></li>
+                    <li><a className="dropdown-item" href="#">Compact</a></li>
+                    <li><a className="dropdown-item" href="#">SUV</a></li>
                   </ul>
 
-                  <button type="button" class="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                    Pricing<span class="visually-hidden">Toggle Dropdown</span>
+                  <button type="button" className="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                    Pricing<span className="visually-hidden">Toggle Dropdown</span>
                   </button>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Sedan</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    <li><a class="dropdown-item" href="#">Separated link</a></li>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Give me range</a></li>
+                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    <li><a className="dropdown-item" href="#">Separated link</a></li>
                   </ul>
 
-                  <input type="text" class="form-control" aria-label="Text input with segmented dropdown button"/>
-
-
+                  <input type="text" className="form-control" aria-label="Text input with segmented dropdown button" />
+                
+                    <button className="btn btn-outline-secondary" type="button">Search</button>
+               
                 </div>
-
-
-
-
-
-
-
-
-
               </div>
 
               <div className="left-map">
