ab3054170b5347297a400379ed25a69e7edcb426
Stiven Kang <kdaegyung@gmail.com>
2023-03-10 20:28:40 -0500

    Display rate

--- frontend/src/components/UserProfile/SpotItem.js
+++ frontend/src/components/UserProfile/SpotItem.js
@@ -17,7 +17,7 @@ const SpotItem = ({ spot }) => {
     const handleEditClick = (e) => {
 		e.preventDefault();
 		history.push({
-			pathname:`/spots/${spot._id}`,
+			pathname:`/spots/create/${spot._id}`,
 			state: {spot: spot}
 		});
 		// dispatch(updateSpot(spot._id));
@@ -31,12 +31,19 @@ const SpotItem = ({ spot }) => {
                     <p>{spot.address}</p>
                     <p>{spot.city}{", "} {spot.state}{", "}{spot.zip}</p>
                 </div>
+                <div>
+                    <h1>Availability:</h1>
+                    <p>{spot.owner.date}</p>
+                    {/* <p><span>Availability: </span>{spot.date}</p> */}
+                </div>
+                <div>
+                    <h1>Rate:</h1>
+                    <p>${spot.rate}/hr</p>
+                    {/* <p><span>Rate: </span>${spot.rate}/hr</p> */}
+                </div>
             </div>
             <div className="right-side-res">
-                <div className="reservation-dates">
-                    <h1>Availability:</h1>
-                    <p><span>Rate: </span>${spot.rate}/hr</p>
-                </div>
+                {/* insert images on right side */}
             </div>
             <div className="edit-wrapper">
 				<FontAwesomeIcon icon={faPenToSquare} className="edit-icon" onClick={handleEditClick}/>
