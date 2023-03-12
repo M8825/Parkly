5f56d076276e1bf37e852089316c52a538b78c47
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-03-11 21:55:12 -0500

    spot delete route now deletes associated dependent reservations

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -115,8 +115,9 @@ router.delete("/:id", requireUser, async (req, res, next) => {
 			const keys = imageUrls.map((url) => {
 				return { Key: url.split("/").pop() };
 			});
-			console.log(keys);
-			await deleteFiles(keys);
+			// console.log(keys);
+			// await deleteFiles(keys);
+			Reservation.deleteMany({spot: spot._id});
 			spot = await Spot.deleteOne({ _id: spot._id });
 			return res.json(spot);
 		} else {
